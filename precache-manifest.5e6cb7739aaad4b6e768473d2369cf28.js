2b2a49d9b624d43645e3bb682fa139555f67b11f
Lev Vasilyev <torikowashi@gmail.com>
2020-06-17 19:38:43 +0300

    Updates

---
+++ precache-manifest.5e6cb7739aaad4b6e768473d2369cf28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95993ad147d348807f3c7f6ad7bb4054",
+    "url": "/not-to-scale-td/index.html"
+  },
+  {
+    "revision": "e48523a86fd50c8c1ace",
+    "url": "/not-to-scale-td/static/css/main.2397e1b0.chunk.css"
+  },
+  {
+    "revision": "d23e9cd84e6c743c3875",
+    "url": "/not-to-scale-td/static/js/2.710a6966.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/not-to-scale-td/static/js/2.710a6966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48523a86fd50c8c1ace",
+    "url": "/not-to-scale-td/static/js/main.879f5073.chunk.js"
+  },
+  {
+    "revision": "0eb8102c77397390231e",
+    "url": "/not-to-scale-td/static/js/runtime-main.999b8b20.js"
+  },
+  {
+    "revision": "3ede88a197a14d38824e15bfb3a5d9d2",
+    "url": "/not-to-scale-td/static/media/logoVI.3ede88a1.svg"
+  }
+]);

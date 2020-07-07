1892397208892c745ca7a5b83ad099ffd962d42a
Lev Vasilyev <torikowashi@gmail.com>
2020-07-07 11:44:00 +0300

    style admin panel

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { format } from 'date-fns'
+import { formatISO9075 } from 'date-fns'
 import CSVReader from 'react-csv-reader'
 import fetch from 'isomorphic-unfetch'
 
@@ -14,14 +14,15 @@ class Admin extends React.Component {
     this.state = {
       data: [],
       password: "",
-      loggedIn: true,
+      loggedIn: false,
       file: undefined,
     }
   }
 
   loadData = async () => {
     this.setState({
-      data: await (await fetch("https://schedule.tochkadostupa.spb.ru/api/nottoscaleticket?limit=5000")).json()
+      data: await (await fetch("https://schedule.tochkadostupa.spb.ru/api/nottoscaleticket?limit=5000")).json(),
+      users: await (await fetch("https://schedule.tochkadostupa.spb.ru/api/nottoscaleuser?limit=5000")).json(),
     })
     // console.log(await (await fetch("https://schedule.tochkadostupa.spb.ru/api/nottoscaleticket?limit=5000")).json())
   }
@@ -61,7 +62,7 @@ class Admin extends React.Component {
     const { password, loggedIn, data } = this.state
 
     return !loggedIn ?
-      <div className="ZoomTicket">
+      <div className="admin">
         <div className="container">
           <Input
             value={password}
@@ -74,31 +75,52 @@ class Admin extends React.Component {
         </div>
       </div>
     :
-      <div className="ZoomTicket">
-        <div className="container">
-          загрузка .CSV файла (все данные будут перезаписаны)
-          <CSVReader
-            onFileLoaded={(data, fileInfo) => this.addData(data)}
-          />
+      <div className="admin">
+        <div className="admin__file">
+          <div className="container">
+            загрузка .CSV файла (все данные будут перезаписаны)
+            <CSVReader
+              onFileLoaded={(data, fileInfo) => this.addData(data)}
+            />
+          </div>
         </div>
-        <div className="container">
-          <div className="ZoomTicket__tickets">
-            <h4 className="h4">Текущие билеты:</h4>
-            {data.map((ticket, index) =>
-              <div
-                key={ticket.id}
-                className="ZoomTicket__tickets__item"
-              >
-                <div className="ZoomTicket__tickets__item__index">
-                  {index + 1}.
+
+        <div className="admin__tickets">
+          <div className="container">
+            {data
+              .map(ticket => ({
+                ...ticket,
+                user0: ticket.user0 && this.state.users.find(user => user.id === ticket.user0),
+                user1: ticket.user1 && this.state.users.find(user => user.id === ticket.user1),
+              }))
+              .map((ticket, index) =>
+                <div
+                  key={ticket.id}
+                  className="admin__tickets__item"
+                >
+                  <div className="admin__tickets__item__index">
+                    {index + 1}.
+                  </div>
+                  <div className="admin__tickets__item__number">
+                    {ticket.number}
+                  </div>
+                  <div className="admin__tickets__item__used">
+                    {ticket.user0 && ticket.user1 ?
+                      ticket.used ?
+                        "закончил"
+                        :
+                        "в процессе"
+                      :
+                      "--"
+                    }
+                  </div>
+                  <div className="admin__tickets__item__user--0">
+                    {ticket.user0 ? `вошёл ${formatISO9075(new Date(ticket.user0.logged), 'dd/mm/yyyy').replace('2020-', '').replace('07-', '')}` : "--"}
+                  </div>
+                  <div className="admin__tickets__item__user--1">
+                    {ticket.user1 ? `вошёл ${formatISO9075(new Date(ticket.user1.logged), 'dd/mm/yyyy').replace('2020-', '').replace('07-', '')}` : "--"}
+                  </div>
                 </div>
-                <div className="ZoomTicket__tickets__item__ticketNumber">
-                  {ticket.number}
-                </div>
-                <div className="ZoomTicket__tickets__item__zoomURL">
-                  {ticket.used ? format(new Date(ticket.used * 1000), 'dd/mm/yyyy') : "не активирован"}
-                </div>
-              </div>
             )}
           </div>
         </div>
